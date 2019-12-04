07d7a92db9396e72645ef3b7317cefcbc1c52edc
Troels Ugilt Jensen <tujensen@gmail.com>
2019-12-04 10:28:05 +0100

    DEVSUPP-120: Fixed broken koba resource picker

--- DependencyInjection/ItkKobaIntegrationExtension.php
+++ DependencyInjection/ItkKobaIntegrationExtension.php
@@ -2,27 +2,23 @@
 
 namespace Itk\KobaIntegrationBundle\DependencyInjection;
 
+use Os2Display\CoreBundle\DependencyInjection\Os2DisplayBaseExtension;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\Config\FileLocator;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
-use Symfony\Component\DependencyInjection\Loader;
 
 /**
  * This is the class that loads and manages your bundle configuration
  *
  * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
  */
-class ItkKobaIntegrationExtension extends Extension
+class ItkKobaIntegrationExtension extends Os2DisplayBaseExtension
 {
     /**
      * {@inheritdoc}
      */
     public function load(array $configs, ContainerBuilder $container)
     {
-        $configuration = new Configuration();
-        $config = $this->processConfiguration($configuration, $configs);
+        $this->dir = __DIR__;
 
-        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
-        $loader->load('services.yml');
+        parent::load($configs, $container);
     }
 }
